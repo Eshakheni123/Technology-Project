4de55503864e49d9e0cadce329534456981df2bc
Eshakheni123 <52167386+Eshakheni123@users.noreply.github.com>
2019-06-25 14:29:30 +0200

    project setup

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JigneshWebApplication1.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JigneshWebApplication1.Startup))]
+namespace JigneshWebApplication1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
